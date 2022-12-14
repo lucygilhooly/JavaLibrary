a587ed97f7ac78f1d3554cf5eda79dde9c05d13d
lucy gilhooly <lucy_johannah@hotmail.com>
2022-12-14 11:53:05 +0000

    inital commit

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,58 @@
+package org.example;
+
+public class Book {
+    private String title;
+    private String author;
+    private int length;
+    private String genre;
+    private String publisher;
+
+    public Book(String title, String author, int length, String genre, String publisher) {
+        this.title = title;
+        this.author = author;
+        this.length = length;
+        this.genre = genre;
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+}
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class User {
+    // name, membership no, age, fav genre, books checked out
+    private String name;
+    private Double membershipNum;
+    private int age;
+    private String favGenre;
+    private int numOfBooks;
+}
